03ab5371f3f3b400b8f7e77795df3f93b1966066
Mehedi Hasan Shamrat <mhshamrat11@gmail.com>
2025-06-04 13:23:59 +0600

    Some code are removed

--- bin/untitled1.dart
+++ bin/untitled1.dart
@@ -1,18 +1,32 @@
-void main(){
+void main() {
+  int ab = 125;
+  String names = 'Mehedi';
+  String named = 'Hasan';
 
-  print('Hello My First Project..');
-  print('Hello My First Project..');
-  print('Hello My First Project..');
-  print('Hello My First Project..');
-  print('Hello My First Project..');
+  print('Hello My First Project..01');
+  print('Hello My First Project..02');
+  print('Hello My First Project..03');
+  print('Hello My First Project..04');
+  print('Hello My First Project..05');
 
-
+  print(named);
+  print(names);
+  print(ab);
 }
 
-void anotherMethod(int a){
-  print('Hello Method');
-}
+  // anotherMethod(10);
+  // anotheroneMethod(20);
 
-void anotheroneMethod(int b){
-  print('Hello Method');
-}
+//
+//
+// void anotherMethod(int a){
+//   print('Hello Method');
+// }
+//
+// void anotheroneMethod(int b){
+//   print('Hello Method');
+// }
+//
+// class myClass{
+//
+// }
